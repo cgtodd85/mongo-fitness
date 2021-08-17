ed3be4555914b0f271db37accb22b5117f1a5f70
Connor Todd <cgtodd85@gmail.com>
2021-08-17 17:21:37 -0400

    workoutjs model

---
+++ models/workout.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now(),
+  },
+  exercises: [
+    {
+      type: { type: String },
+      name: String,
+      distance: Number,
+      duration: Number,
+      weight: Number,
+      sets: Number,
+      reps: Number,
+    },
+  ],
+});
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
