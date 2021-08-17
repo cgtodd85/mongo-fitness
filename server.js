2b0e4edb1722b03e0a64dc6743b7a5f9fd01634d
Connor Todd <cgtodd85@gmail.com>
2021-08-17 19:34:58 -0400

    updates to server js

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
