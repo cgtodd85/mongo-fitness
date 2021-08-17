e6a2c58952110dd9bc2d32b63051b647c61ce18b
Connor Todd <cgtodd85@gmail.com>
2021-08-17 17:16:00 -0400

    updated workout routes main directory

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -1,3 +1,17 @@
+const router = require("express").Router();
+const { Workout } = require("../../models");
 
-const router = require('express').Router();
-const { Workout } = require('../../models');
+router.get("/", (req, res) =>
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: "$exercises.duration",
+        },
+      },
+    },
+  ])
+    .sort({ date: -1 })
+    .then((dbWorkout) => res.json(dbWorkout))
+    .catch((err) => res.status(400).json(err))
+);
